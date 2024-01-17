bc38f55ddecfe349289e86772912457d82b47af9
Marcus Doucette <Marz3587@gmail.com>
2024-01-17 10:44:50 -0500

    01/16 changes

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 /*
 Tasks
-  - refactor input into convinience methods (template)
   - add priority enumeration
-  - 
+  - add options to pick an individual project to project view
+  - figure out how to beter handle error cases in get_input 
+  - impliment some validation layers across app
+  
+  - way to edit tasks within projects
+  - way to delete projects
+  - might want some prioritization for tasks.
+  - view learning goals
+  - add learning goals
+
+  - completing tasks workflow
+
+  - priority should be visible on higher order lists
+  - at the point we're sorting tasks projects can be sorted by priority too
+
 */
 
 use std::io::Write;
 use std::io::prelude::*;
 use std::str::FromStr;
+use std::collections::HashMap;
 
 // enum Priority{
 //   NeedToDoIt,
@@ -34,18 +48,22 @@ struct Task{
   name:String,
   description:String,
   completed:bool,
+  priority: u8,
+  relevant_learning_goal_id:u32,
 }
-
 impl Task{
-  fn make_vec_from_cmd() -> Vec<Self>{
+  fn make_vec_from_cmd(data:&mut AppData) -> Vec<Self>{
     let mut out: Vec<Self> = vec![];
     loop{
       out.push(Task{
         name:get_input::<String>(" - task name: "),
-        description:get_input::<String>(" - task description:"),
-        completed:false});
+        description:get_input::<String>(" - task description: "),
+        completed:false,
+        relevant_learning_goal_id:data.pick_learning_goal(),
+        priority:get_input::<u8>(" - priority (0-10): "),
+      });
 
-      let ans = get_input::<String>("Add another task? (y/n)").to_lowercase();
+      let ans = get_input::<String>("Add another task? (y/n): ").to_lowercase();
       if !(ans == "y" || ans == "yes"){
         break;
       }
@@ -54,10 +72,9 @@ impl Task{
   }
 
   fn to_string(&self) -> String{
-    return format!("{1}{0}{2}{0}{3}",INLINE_DELIMITER,self.name,self.description,self.completed)
+    return format!("{1}{0}{2}{0}{3}{0}{4},{0},{5}",INLINE_DELIMITER,self.name,self.description,self.completed,self.relevant_learning_goal_id,self.priority)
   }
 }
-
 struct Project{
   name:String,
   tasks: Vec<Task>,
@@ -76,24 +93,48 @@ impl Project{
     }
   }
 
-  fn make_from_cmd() -> Self{
+  fn make_from_cmd(data:&mut AppData) -> Self{
     println!("Making a new project");
     let name = get_input::<String>(" - name: ");
     let priority = get_input::<u8>(" - priority (0-10): ");
 
-    let tasks = Task::make_vec_from_cmd();
+    let tasks = Task::make_vec_from_cmd(data);
 
     return Project { name, 
       tasks, 
       priority}
   }
 
-  fn load(filename: &str)->Vec<Self>{
-    let mut out:Vec<Self> = vec![];
+  fn to_string(&self)->String{
+    return format!("{1}{0}{2}{0}{3}",INLINE_DELIMITER,self.name,self.tasks.len(),self.priority);
+  }
+}
+
+struct LearningGoal{
+  id: u32,
+  parent: u32, // do we need more than one?
+  goal: String,
+  priority:u8,
+  tasks_completed: u32,
+  //resources: Vec<String>,
+}
+impl LearningGoal{
+  fn to_string(&self) -> String{
+    return format!("{1},{0},{2},{0},{3},{0},{4},{0},{5}",INLINE_DELIMITER,self.id,self.parent,self.goal,self.priority,self.tasks_completed);
+  }
+}
+
+struct AppData{
+  filename:String,
+  projects:Vec<Project>,
+  learning_goals:HashMap<u32,LearningGoal>
+}
+impl AppData{
+  fn load(&mut self){
     let mut file;
-    match std::fs::File::open(&filename){
+    match std::fs::File::open(&self.filename){
       Ok(f)=>{file = f},
-      Err(_)=>{return out}
+      Err(_)=>{todo!()} // TODO better error handling
     };
     let mut filelines = String::new();
     match file.read_to_string(&mut filelines).map_err(|err| println!("{}",err)){
@@ -108,15 +149,27 @@ impl Project{
               let name:String = part[0].trim().to_owned();
               let description: String = part[1].trim().to_owned();
               let completed:bool = part[2].trim().parse::<bool>().unwrap();
-              tasks.push(Task{name,description,completed})
-            }
+              let priority:u8 = part[3].trim().parse::<u8>().unwrap();
+              let relevant_learning_goal_id:u32 = part[4].trim().parse::<u32>().unwrap();
+              tasks.push(Task{name,description,completed,priority,relevant_learning_goal_id})
+            },
             "p_" => {       
               let ln = line[2..].to_owned();   
               let part: Vec<&str> = ln.trim().split(INLINE_DELIMITER).collect();
               let name:String = part[0].trim().to_owned();
               let priority: u8 = part[1].trim().parse::<u8>().unwrap();
-              out.push(Project {name,tasks,priority});
+              self.projects.push(Project{name,tasks,priority});
               tasks = vec![];
+            },
+            "l_" => {
+              let ln = line[2..].to_owned();
+              let part: Vec<&str> = ln.trim().split(INLINE_DELIMITER).collect();
+              let id = part[0].trim().parse::<u32>().unwrap();
+              let parent = part[1].trim().parse::<u32>().unwrap();
+              let goal = part[2].trim().to_owned();
+              let priority = part[3].trim().parse::<u8>().unwrap();
+              let tasks_completed = part[4].trim().parse::<u32>().unwrap();
+              self.learning_goals.insert(id,LearningGoal{id,parent,goal,priority,tasks_completed});
             }
             _=>{}
           }
@@ -124,56 +177,71 @@ impl Project{
       }},
       Err(_) => {}
     }
-    
-    return out;
   }
-  fn save(list: &[Self],filename:&str){
+  fn save(&mut self){
     let mut filelines = String::new();
-    for item in list{
+    for item in &self.projects{
       for task in &item.tasks{
         filelines += "t_";
         filelines += &task.to_string();
         filelines += LINE_BREAK_DELIMITER;
       }
       filelines += "p_";
-      filelines += &item.to_string() as &str;
+      filelines += &item.to_string();
+      filelines += LINE_BREAK_DELIMITER;
+    }
+    for (_, item) in &self.learning_goals{
+      filelines += "l_";
+      filelines += &(item.to_string());
       filelines += LINE_BREAK_DELIMITER;
     }
 
-    let mut file = std::fs::File::create(&filename).unwrap();
+    let mut file = std::fs::File::create(&self.filename).unwrap();
     file.write_all(filelines.as_bytes()).ok();
-
   }
-  fn to_string(&self)->String{
-    return format!("{1}{0}{2}{0}{3}",INLINE_DELIMITER,self.name,self.tasks.len(),self.priority);
+  fn pick_learning_goal(&mut self) -> u32{
+    println!("Pick a Goal:");
+    println!(" 0 - None");
+    for (id,goal) in &self.learning_goals{
+      println!(" {} - {}",id,goal.goal);
+    }
+    return get_input::<u32>("");
   }
 }
 
-
 fn main() {
   let filename = "Test.proj".to_owned();
-  let mut projects:Vec<Project> = Project::load(&filename);
+  let mut data:AppData = AppData{filename,projects:vec![],learning_goals:HashMap::new()};
+  data.load();
   loop {
     println!("Please select an option:");
     println!(" 0 - save & quit");
     println!(" 1 - view projects");
     println!(" 2 - add new Project");
-    //println!(" 3 - load from file");
+    println!(" 3 - view learning goals");
+    println!(" 4 - add new learning goal");
+    println!(" 5 - save");
     let mut line = String::new();
     let stdin = std::io::stdin();
     stdin.read_line(&mut line).map_err(|err| println!("{}",err)).ok();
     if let Ok(answer) = line.trim().parse::<i32>(){
       match answer{
         0 => {
-          Project::save(&projects,&filename);
+          data.save();
           return
         },
-        1 => Project::view(&projects),
+        1 => Project::view(&data.projects),
         2 => {
-          let project = Project::make_from_cmd();
+          let project = Project::make_from_cmd(&mut data);
           println!("{}",project.to_string());
-          projects.push(project); 
+          data.projects.push(project);
+          
         },
+        3 => {},
+        4 => {},
+        5 => {
+          data.save();
+        }
         _ => println!("Command not recognized")
       }
 
